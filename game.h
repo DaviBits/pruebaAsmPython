0e64e0e6891d25e807991486146ea65148d06734
Artemio-lab <villalobos1199190@uabc.edu.mx>
2025-12-02 17:15:51 -0800

    agregado de funciones masm, basicas sobre cadenas

--- game.h
+++ game.h
@@ -2,5 +2,8 @@
 #define _GAME_
 
 int rnd(int max);
-
+int cmpCad(char cad1[], char cad2[]);
+int charInCad(char car, char cad[]);
+int lenCad(char cad[]);
+void mezclarCadena(char cad[]);
 #endif
--- probador.py
+++ probador.py
@@ -6,3 +6,8 @@ lib = _game_lib.lib
 random=lib.rnd(10)
 
 print("numero random del 1 al 10: ", random)
+
+cad1 = "holas amigos"
+cad2="hola amigos"
+car="a"
+print(lib.charInCad(cad1,car))
